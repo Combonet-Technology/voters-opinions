5fe0307d872081fc4e267d9979308c649a366d12
bcx <femolak@gmail.com>
2022-09-11 00:22:34 +0100

    updates

--- app.js
+++ app.js
@@ -1,63 +1,88 @@
-const path = require('path');
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const morgan = require('morgan');
-const passport = require('passport');
-// const session = require('cookie-session');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-// const expressHBS = require('express-handlebars');
-const { engine } = require('express-handlebars');
-const { default: mongoose } = require('mongoose');
-
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const morgan = require("morgan");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const { engine } = require("express-handlebars");
+const { default: mongoose } = require("mongoose");
 
 // load config
-dotenv.config({path: './config/.env'});
+dotenv.config({ path: "./config/.env" });
 connectDB();
 const app = express();
 
 // body parser middleware
-app.use(express.urlencoded({extended:false}));
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // logging
-if (process.env.NODE_ENV === 'development'){
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // load passport
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
-//handlebars 
+//handlebars
+// Handlebars Helpers
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require("./helpers/hbs");
+
 // app.engine('.hbs', expressHBS.engine({defaultLayout:'main', extname: '.hbs'}));
-app.engine('.hbs', engine({defaultLayout:'main', extname: '.hbs'}));
-app.set('view engine', '.hbs');
-app.set('views', './views');
+app.engine(
+  ".hbs",
+  engine({
+    defaultLayout: "main",
+    extname: ".hbs",
+    helpers: {
+      formatDate,
+      stripTags,
+      truncate,
+      editIcon,
+      select,
+    },
+  })
+);
+app.set("view engine", ".hbs");
+app.set("views", "./views");
 
 // static settings
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-// session middleware 
-app.use(session({
-    secret: 'keyboard cat',
+// session middleware
+app.use(
+  session({
+    secret: "keyboard cat",
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({
-      mongoUrl: process.env.MONGO_URI,})
-  }));
+      mongoUrl: process.env.MONGO_URI,
+    }),
+  })
+);
 
-  // passport middleware
-  app.use(passport.initialize());
-  app.use(passport.session());
+// passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // load passport
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // base routes
-app.use('/', require('./routes/index'));
-app.use('/auth', require('./routes/auth'));
-app.use('/opinions', require('./routes/opinions'));
+app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
+app.use("/opinions", require("./routes/opinions"));
 
-const PORT = process.env.PORT || 5000; 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
--- routes/opinions.js
+++ routes/opinions.js
@@ -1,160 +1,165 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {ensureAuth} = require('../middleware/auth');
+const { ensureAuth } = require("../middleware/auth");
+const Opinions = require("../models/Opinions");
 
-// @desc    Show add page
-// @route   GET /stories/add
-router.get('/add', ensureAuth, (req, res) => {
-    res.render('opinions/add')
-  });
+// @desc    render add page
+// @route   GET /opinions/add
+router.get("/add", ensureAuth, (req, res) => {
+  res.render("opinions/add");
+});
 
-  // @desc    Process add form
-// @route   POST /stories
-router.post('/', ensureAuth, async (req, res) => {
-    try {
-      req.body.user = req.user.id
-      await Story.create(req.body)
-      res.redirect('/dashboard')
-    } catch (err) {
-      console.error(err)
-      res.render('error/500')
-    }
-  })
-  
-  // @desc    Show all stories
-  // @route   GET /stories
-  router.get('/', ensureAuth, async (req, res) => {
-    try {
-      const stories = await Story.find({ status: 'public' })
-        .populate('user')
-        .sort({ createdAt: 'desc' })
-        .lean()
-  
-      res.render('stories/index', {
-        stories,
-      })
-    } catch (err) {
-      console.error(err)
-      res.render('error/500')
-    }
-  })
-  
-  // @desc    Show single story
-  // @route   GET /stories/:id
-  router.get('/:id', ensureAuth, async (req, res) => {
-    try {
-      let story = await Story.findById(req.params.id).populate('user').lean()
-  
-      if (!story) {
-        return res.render('error/404')
-      }
-  
-      if (story.user._id != req.user.id && story.status == 'private') {
-        res.render('error/404')
-      } else {
-        res.render('stories/show', {
-          story,
-        })
-      }
-    } catch (err) {
-      console.error(err)
-      res.render('error/404')
-    }
-  })
-  
-  // @desc    Show edit page
-  // @route   GET /stories/edit/:id
-  router.get('/edit/:id', ensureAuth, async (req, res) => {
-    try {
-      const story = await Story.findOne({
-        _id: req.params.id,
-      }).lean()
-  
-      if (!story) {
-        return res.render('error/404')
-      }
-  
-      if (story.user != req.user.id) {
-        res.redirect('/stories')
-      } else {
-        res.render('stories/edit', {
-          story,
-        })
-      }
-    } catch (err) {
-      console.error(err)
-      return res.render('error/500')
-    }
-  })
-  
-  // @desc    Update story
-  // @route   PUT /stories/:id
-  router.put('/:id', ensureAuth, async (req, res) => {
-    try {
-      let story = await Story.findById(req.params.id).lean()
-  
-      if (!story) {
-        return res.render('error/404')
-      }
-  
-      if (story.user != req.user.id) {
-        res.redirect('/stories')
-      } else {
-        story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
-          new: true,
-          runValidators: true,
-        })
-  
-        res.redirect('/dashboard')
-      }
-    } catch (err) {
-      console.error(err)
-      return res.render('error/500')
-    }
-  })
-  
-  // @desc    Delete story
-  // @route   DELETE /stories/:id
-  router.delete('/:id', ensureAuth, async (req, res) => {
-    try {
-      let story = await Story.findById(req.params.id).lean()
-  
-      if (!story) {
-        return res.render('error/404')
-      }
-  
-      if (story.user != req.user.id) {
-        res.redirect('/stories')
-      } else {
-        await Story.remove({ _id: req.params.id })
-        res.redirect('/dashboard')
-      }
-    } catch (err) {
-      console.error(err)
-      return res.render('error/500')
-    }
-  })
-  
-  // @desc    User stories
-  // @route   GET /stories/user/:userId
-  router.get('/user/:userId', ensureAuth, async (req, res) => {
-    try {
-      const stories = await Story.find({
-        user: req.params.userId,
-        status: 'public',
-      })
-        .populate('user')
-        .lean()
-  
-      res.render('stories/index', {
-        stories,
-      })
-    } catch (err) {
-      console.error(err)
-      res.render('error/500')
-    }
-  })
-  
+// @desc    Process add form
+// @route   POST /opinions
+router.post("/", ensureAuth, async (req, res) => {
+  try {
+    console.log(req.body);
+    req.body.user = req.user.id;
+    await Opinions.create(req.body);
+    res.redirect("/dashboard");
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
+});
 
+// @desc    Show all opinions
+// @route   GET /opinions
+router.get("/", ensureAuth, async (req, res) => {
+  try {
+    const opinions = await Opinions.find({ status: "public" })
+      .populate("user")
+      .sort({ createdAt: "desc" })
+      .lean();
+    console.log(opinions[0]._id);
+    res.render("opinions/index", {
+      opinions,
+    });
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
+});
+
+// @desc    Show single opinion
+// @route   GET /opinions/:id
+router.get("/:id", ensureAuth, async (req, res) => {
+  try {
+    let selected_opinion = await Opinions.findById(req.params.id)
+      .populate("user")
+      .lean();
+
+    if (!selected_opinion) {
+      return res.render("error/404");
+    }
+
+    if (
+      selected_opinion.user._id != req.user.id &&
+      selected_opinion.status == "private"
+    ) {
+      res.render("error/404");
+    } else {
+      res.render("opinions/show", {
+        opinion: selected_opinion,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    res.render("error/404");
+  }
+});
+
+// @desc    Show edit page
+// @route   GET /opinions/edit/:id
+router.get("/edit/:id", ensureAuth, async (req, res) => {
+  try {
+    const story = await Story.findOne({
+      _id: req.params.id,
+    }).lean();
+
+    if (!story) {
+      return res.render("error/404");
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect("/opinions");
+    } else {
+      res.render("opinions/edit", {
+        story,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    return res.render("error/500");
+  }
+});
+
+// @desc    Update story
+// @route   PUT /opinions/:id
+router.put("/:id", ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).lean();
+
+    if (!story) {
+      return res.render("error/404");
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect("/opinions");
+    } else {
+      story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true,
+      });
+
+      res.redirect("/dashboard");
+    }
+  } catch (err) {
+    console.error(err);
+    return res.render("error/500");
+  }
+});
+
+// @desc    Delete story
+// @route   DELETE /opinions/:id
+router.delete("/:id", ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).lean();
+
+    if (!story) {
+      return res.render("error/404");
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect("/opinions");
+    } else {
+      await Story.remove({ _id: req.params.id });
+      res.redirect("/dashboard");
+    }
+  } catch (err) {
+    console.error(err);
+    return res.render("error/500");
+  }
+});
+
+// @desc    User opinions
+// @route   GET /opinions/user/:userId
+router.get("/user/:userId", ensureAuth, async (req, res) => {
+  try {
+    const opinions = await Story.find({
+      user: req.params.userId,
+      status: "public",
+    })
+      .populate("user")
+      .lean();
+
+    res.render("opinions/index", {
+      opinions,
+    });
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
+});
 
 module.exports = router;
