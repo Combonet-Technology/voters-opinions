79e68ceafe909056711585194dbc7b1b89c6c97b
bcx <femolak@gmail.com>
2022-09-10 20:24:34 +0100

    updates

--- app.js
+++ app.js
@@ -5,14 +5,21 @@ const connectDB = require('./config/db');
 const morgan = require('morgan');
 const passport = require('passport');
 // const session = require('cookie-session');
-const session = require('express-session');// cookie-session
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 // const expressHBS = require('express-handlebars');
 const { engine } = require('express-handlebars');
+const { default: mongoose } = require('mongoose');
+
 
 // load config
 dotenv.config({path: './config/.env'});
 connectDB();
 const app = express();
+
+// body parser middleware
+app.use(express.urlencoded({extended:false}));
+app.use(express.json())
 
 // logging
 if (process.env.NODE_ENV === 'development'){
@@ -20,7 +27,7 @@ if (process.env.NODE_ENV === 'development'){
 }
 
 // load passport
-require('./config/passport')(passport)
+require('./config/passport')(passport);
 
 //handlebars 
 // app.engine('.hbs', expressHBS.engine({defaultLayout:'main', extname: '.hbs'}));
@@ -36,19 +43,21 @@ app.use(session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
-    // cookie: { secure: true, maxAge:60000 }
-  }))
-  
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,})
+  }));
+
   // passport middleware
-  app.use(passport.initialize()) //http://localhost:3000/auth/google/callback
-  app.use(passport.session())
+  app.use(passport.initialize());
+  app.use(passport.session());
 
 // load passport
-require('./config/passport')(passport)
+require('./config/passport')(passport);
 
 // base routes
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
+app.use('/opinions', require('./routes/opinions'));
 
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
